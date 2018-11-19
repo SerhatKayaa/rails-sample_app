6de3b86d2bb76f0b09e0cd584de54951219e70a2
skaya2E <serhat.kaya@fazlagida.com>
2018-11-19 17:38:51 +0300

    account-activation

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,12 +5,31 @@ class SessionController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me]  == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      flash[:danger].now = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
+    end
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,10 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -19,6 +22,6 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password,
-               admin: true)
-
+               activated: true,
+               activated_at: Time.zone.now)
 end
